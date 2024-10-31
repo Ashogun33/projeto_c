26e4fb5894e996a506bfb693f44c439abaaae911
Vandilson Ashogun <ashogun33@gmail.com>
2024-10-30 22:56:08 -0300

    Adiciona saudação especial para Ashogun e saudação padrão para outros nomes

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char nome[50]; /* Declara variável para armazenar um nome */
 
     printf("Olá, mundo! Este é meu primeiro programa em C.\n");
-    printf("Olá! Qual o seu nome?\n");
-    scanf("%49s",nome); /* Captura o nome inserido pelo usuário */
     
-    printf("Olá, %s! Seja bem-vindo ao mundo da programação em C!\n", nome);
-    return 0;
+    printf("Olá! Qual o seu nome?\n");
+    scanf("%49s",nome);
+    
+    if(strcmp(nome, "Ashogun") == 0) {
+       printf("Saudações, Ashogun! é uma honra tê-lo aqui.\n");
+    } else {
+       printf("Olá, %s! Seja bem-vindo ao mundo da programação em C!\n", nome);
+    }
 
+    return 0;
 }
