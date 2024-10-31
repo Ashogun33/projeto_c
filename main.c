5edf4036ee56e942685edc0ccca4817168bae0e5
Vandilson Ashogun <ashogun33@gmail.com>
2024-10-30 22:03:41 -0300

    Primeiro programa em C: Olá Mundo + apresentação pessoal

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 int main() {
     printf("Olá,mundo! Meu primeiro programa em C\n");
+    printf("Olá! Qual o seu nome?\n");
+    printf("Meu nome é Vandilson, mas pode me chamar de Ashogun.\n");
     return 0;
 
 }
